8a9e3007f43eb4aa196d954bc8c62ebba460a03e
MainStar <mainstar313@gmail.com>
2019-08-09 11:02:55 +0300

    Minor fixes.

--- core/src/main/java/epam/core/services/PageSearchService.java
+++ core/src/main/java/epam/core/services/PageSearchService.java
@@ -1,15 +1,12 @@
 package epam.core.services;
 
-
-
-import javax.jcr.NodeIterator;
 import javax.jcr.Session;
 
 public interface PageSearchService {
 
     /**
      * This function return list of values from root path
-     * @param session - Session
+     * @param session - JCR Session
      * @param rootPath - root path for search
      * */
     StringBuilder listOfNodesFromRootPathByKeyword(Session session, String rootPath);
