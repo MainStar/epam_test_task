6eb230f0c9ff29ada8dfe3b22ae24a74563c9f67
MainStar <mainstar313@gmail.com>
2019-08-09 11:11:59 +0300

    Fixed import in QueryBuilderServlet.

--- core/src/main/java/epam/core/servlets/QueryBuilderServlet.java
+++ core/src/main/java/epam/core/servlets/QueryBuilderServlet.java
@@ -12,7 +12,7 @@ import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.jcr.*;
+import javax.jcr.Session;
 import javax.servlet.Servlet;
 import java.io.IOException;
 
